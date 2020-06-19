9e179b0106b48cdc06cb3f5bc3008ef1459da6c8
Iqrasiddiqui07 <63970324+Iqrasiddiqui07@users.noreply.github.com>
2020-06-19 16:30:44 +0530

    positive.py

---
+++ positive.py
@@ -0,0 +1,11 @@
+list1 = [-7, 5, 12, 64, -14]
+print("list1= ", list1)
+for number in list1:
+    if number >0:
+        print(number, end =" ")
+list2 = [12, 14,-95,3]
+print(end ='\n')
+print("list2= ", list2,end ='\n')
+for number in list2:
+    if number >0:
+        print(number, end =" ")
